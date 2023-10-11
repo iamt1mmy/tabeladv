930f061f64b95dce8474b09652b24274395f3578
iamt1mmy <146391141+iamt1mmy@users.noreply.github.com>
2023-10-11 22:57:27 +0300

    Add files via upload

---
+++ tabeladv.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Tabel de adevar al functiei SI");
+        Console.WriteLine("");
+        Console.WriteLine("---------------------");
+        Console.WriteLine("|  X  |  Y  | X & Y |");
+        Console.WriteLine("|-----|-----|-------|");
+
+        for (int X = 0; X <= 1; X++)
+        {
+            for (int Y = 0; Y <= 1; Y++)
+            {
+                int result = X & Y;
+                Console.WriteLine($"|  {X}  |  {Y}  |   {result}   |");
+            }
+        }
+
+        Console.WriteLine("---------------------");
+    }
+}
